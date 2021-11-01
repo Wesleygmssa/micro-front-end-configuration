5c9fd6902275eba2164fe7a534456db7545ced25
Wesley Guerra <wesley.moreira@compasso.com.br>
2021-11-01 11:12:46 -0300

    configuração das rotas

--- single-spa/src/mc-root-config.js
+++ single-spa/src/mc-root-config.js
@@ -1,18 +1,18 @@
 import { registerApplication, start } from "single-spa";
 
-// registerApplication({
-//   name: "@single-spa/welcome",
-//   app: () =>
-//     System.import(
-//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-//     ),
-//   activeWhen: ["/"],
-// });
+registerApplication({
+  name: "@single-spa/welcome",
+  app: () =>
+    System.import(
+      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+    ),
+  activeWhen: (location) => location.pathname === '/'
+});
 
 registerApplication({
   name: "@mc/react-single",
   app: () => System.import("@mc/react-single"),
-  activeWhen: ["/react"]
+  activeWhen: (location) => location.pathname === '/react'
 });
 
 start({
